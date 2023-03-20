6488a77104aa16652450b87246aa12a9728c1760
Ryokusasa <mattya.mqdm@gmail.com>
2023-03-20 18:46:05 +0900

    <refactor>: json→Itemへの変換を関数化
    可読性向上のため

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
@@ -34,7 +34,6 @@ class RepositoriesViewModel(
             }
 
             val jsonBody = JSONObject(response.receive<String>())
-
             val jsonItems = jsonBody.optJSONArray("items")!!
 
             val items = mutableListOf<Item>()
@@ -44,31 +43,34 @@ class RepositoriesViewModel(
              */
             for (i in 0 until jsonItems.length()) {
                 val jsonItem = jsonItems.optJSONObject(i)!!
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                items.add(
-                    Item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = context.getString(R.string.written_language, language),
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
-                    )
-                )
+                val item = json2Item(jsonItem)
+                items.add(item)
             }
 
             lastSearchDate = Date()
 
             return@async items.toList()
         }.await()
+    }
+
+    private fun json2Item(jsonItem: JSONObject): Item {
+        val name = jsonItem.optString("full_name")
+        val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
+        val language = jsonItem.optString("language")
+        val stargazersCount = jsonItem.optLong("stargazers_count")
+        val watchersCount = jsonItem.optLong("watchers_count")
+        val forksCount = jsonItem.optLong("forks_conut")
+        val openIssuesCount = jsonItem.optLong("open_issues_count")
+
+        return Item(
+            name,
+            ownerIconUrl,
+            context.getString(R.string.written_language, language),
+            stargazersCount,
+            watchersCount,
+            forksCount,
+            openIssuesCount
+        )
     }
 }
 
