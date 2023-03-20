9f2a8a288e056914b7766a018799f3765dcf55c4
Ryokusasa <mattya.mqdm@gmail.com>
2023-03-20 18:49:40 +0900

    <refactor>: スペルミス修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
@@ -56,7 +56,7 @@ class RepositoriesViewModel(
         val language = jsonItem.optString("language")
         val stargazersCount = jsonItem.optLong("stargazers_count")
         val watchersCount = jsonItem.optLong("watchers_count")
-        val forksCount = jsonItem.optLong("forks_conut")
+        val forksCount = jsonItem.optLong("forks_count")
         val openIssuesCount = jsonItem.optLong("open_issues_count")
 
         return Item(
