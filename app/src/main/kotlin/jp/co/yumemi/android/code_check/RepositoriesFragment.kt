dd92e0a99754ddbe5998de3e64554e24d99df97a
Ryokusasa <mattya.mqdm@gmail.com>
2023-03-21 23:31:51 +0900

    refactor: 検索イベントをViewModelで処理
    BindingAdapterでEditorActionを指定できるように
    これに伴いinitSearchInputTextは削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
@@ -7,12 +7,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
-import com.google.android.material.textfield.TextInputEditText
 import jp.co.yumemi.android.code_check.databinding.RepositoriesFragmentBinding
 
 class RepositoriesFragment: Fragment(R.layout.repositories_fragment){
@@ -41,22 +39,7 @@ class RepositoriesFragment: Fragment(R.layout.repositories_fragment){
     }
 
     private fun initViews(){
-        with(binding){
-            initSearchInputText(searchInputText)
-            initRepositoriesRecycler(repositoriesRecycler)
-        }
-    }
-
-    private fun initSearchInputText(searchInputText: TextInputEditText){
-        searchInputText.setOnEditorActionListener{ editText, action, _ ->
-                if (action == EditorInfo.IME_ACTION_SEARCH){
-                    UtilCommon.hideSoftKeyBoard(requireContext(), editText)
-                    editText.clearFocus()
-                    viewModel.searchRepositories()
-                    return@setOnEditorActionListener true
-                }
-                return@setOnEditorActionListener false
-            }
+        initRepositoriesRecycler(binding.repositoriesRecycler)
     }
 
     private fun initRepositoriesRecycler(repositoriesRecycler: RecyclerView){
@@ -88,8 +71,6 @@ class RepositoriesFragment: Fragment(R.layout.repositories_fragment){
             .actionRepositoriesFragmentToRepositoryFragment(repository)
         findNavController().navigate(action)
     }
-
-
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
@@ -4,9 +4,13 @@
 package jp.co.yumemi.android.code_check
 
 import android.app.Application
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.textfield.TextInputEditText
 import jp.co.yumemi.android.code_check.UtilCommon.Companion.lastSearchDate
 import kotlinx.coroutines.*
 import org.json.JSONException
@@ -15,11 +19,29 @@ import java.util.*
 class RepositoriesViewModel(
     application: Application
 ) : AndroidViewModel(application) {
+
+    object RepositoriesBindingAdapter {
+        @BindingAdapter("onEditorAction")
+        @JvmStatic
+        fun setOnInputEditorAction(view: TextInputEditText, listener: OnEditorActionListener){
+            view.setOnEditorActionListener(listener)
+        }
+    }
+
     private val context = application
 
     val repositories = MutableLiveData<List<Repository>>()
-
     val searchText = MutableLiveData("")
+
+    val onEditorAction = OnEditorActionListener{editText, action, _ ->
+        if (action == EditorInfo.IME_ACTION_SEARCH){
+            UtilCommon.hideSoftKeyBoard(context, editText)
+            editText.clearFocus()
+            searchRepositories()
+            return@OnEditorActionListener true
+        }
+        return@OnEditorActionListener false
+    }
 
     /** リポジトリ検索
      * 検索失敗時はリポジトリを空にし、エラートーストを表示
