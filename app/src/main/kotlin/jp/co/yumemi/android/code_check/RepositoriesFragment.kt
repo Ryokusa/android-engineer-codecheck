4028bb8facc28a13c01d566e061a0c93cf5daf8d
Ryokusasa <mattya.mqdm@gmail.com>
2023-03-21 16:32:55 +0900

    refactor: RepositoryAdapterを別ファイル定義

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
@@ -111,45 +111,5 @@ class RepositoriesFragment: Fragment(R.layout.repositories_fragment){
     }
 }
 
-val diffUtil = object: DiffUtil.ItemCallback<Repository>(){
-    override fun areItemsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
-    {
-        return oldRepository.name == newRepository.name
-    }
 
-    override fun areContentsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
-    {
-        return oldRepository == newRepository
-    }
-}
 
-class RepositoryAdapter(
-    private val repositoryClickListener: OnItemClickListener,
-) : ListAdapter<Repository, RepositoryAdapter.ViewHolder>(diffUtil){
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
-
-    interface OnItemClickListener{
-    	fun repositoryClick(repository: Repository)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-    {
-    	val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.repository_item_layout, parent, false)
-    	return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int)
-    {
-    	val repository = getItem(position)
-        val itemView = holder.itemView
-
-        val repositoryNameView = itemView.findViewById<TextView>(R.id.repository_name_view)
-        repositoryNameView.text = repository.name
-
-        itemView.setOnClickListener{
-     		repositoryClickListener.repositoryClick(repository)
-    	}
-    }
-}
