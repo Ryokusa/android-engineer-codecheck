afb91f4b20ffc4beaed8c0f3445191129b3d823d
Ryokusasa <mattya.mqdm@gmail.com>
2023-03-22 15:17:11 +0900

    refactor: リポジトリアダプターをリファクター
    変数の名称変更や各関数・クラスdocの記述、またdiffUtilはクラス外の定義でネストを浅く

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryAdapter.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryAdapter.kt
@@ -8,48 +8,65 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import jp.co.yumemi.android.code_check.databinding.RepositoryItemBinding
 
+/** リポジトリアダプター
+ * リポジトリの表示処理を行う
+ * @param repositoryClickListener リポジトリクリックイベント
+ * @param viewLifeCycleOwner viewのライフサイクルオーナー
+ */
 class RepositoryAdapter(
     private val repositoryClickListener: OnItemClickListener,
     private val viewLifeCycleOwner: LifecycleOwner,
-) : ListAdapter<Repository, RepositoryAdapter.ViewHolder>(diffUtil){
+) : ListAdapter<Repository, RepositoryAdapter.RepositoryItemViewHolder>(diffUtil){
 
-    companion object{
-        val diffUtil = object: DiffUtil.ItemCallback<Repository>(){
-            override fun areItemsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
-            {
-                return oldRepository.name == newRepository.name
-            }
-
-            override fun areContentsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
-            {
-                return oldRepository == newRepository
-            }
-        }
-    }
-
-    class ViewHolder(private val binding: RepositoryItemBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(item: Repository, repositoryClickListener: OnItemClickListener, viewLifeCycleOwner: LifecycleOwner) {
+    /** リポジトリのViewHolderクラス
+     * @param binding リポジトリアイテムのbinding
+     */
+    class RepositoryItemViewHolder(private val binding: RepositoryItemBinding): RecyclerView.ViewHolder(binding.root){
+        /** binding設定
+         * RepositoryItemBindingに各情報を格納する
+         * @param repository 表示対象リポジトリ
+         * @param repositoryClickListener リポジトリクリックリスナー
+         * @param viewLifeCycleOwner viewのライフサイクルオーナー
+         */
+        fun bind(repository: Repository, repositoryClickListener: OnItemClickListener, viewLifeCycleOwner: LifecycleOwner) {
             binding.lifecycleOwner = viewLifeCycleOwner
-            binding.repository = item
+            binding.repository = repository
             binding.itemClickListener = repositoryClickListener
 
             binding.executePendingBindings()
         }
     }
 
+    /**　リポジトリクリックリスナー　*/
     interface OnItemClickListener{
+        /** リポジトリクリックイベント
+         *  @param repository クリックされたリポジトリ情報
+         */
         fun repositoryClick(repository: Repository)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryItemViewHolder
     {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return ViewHolder(RepositoryItemBinding.inflate(layoutInflater, parent, false))
+        return RepositoryItemViewHolder(RepositoryItemBinding.inflate(layoutInflater, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    override fun onBindViewHolder(holder: RepositoryItemViewHolder, position: Int)
     {
         val repository = getItem(position)
         holder.bind(repository, repositoryClickListener, viewLifeCycleOwner)
+    }
+}
+
+/** Repositoryアダプター用差分取得関数 */
+private val diffUtil = object: DiffUtil.ItemCallback<Repository>(){
+    override fun areItemsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
+    {
+        return oldRepository.name == newRepository.name
+    }
+
+    override fun areContentsTheSame(oldRepository: Repository, newRepository: Repository): Boolean
+    {
+        return oldRepository == newRepository
     }
 }
